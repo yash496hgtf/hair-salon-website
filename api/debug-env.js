d0d93f7605eb04a285a3a255a1f00b12e13d5ea3
Yashik <kiren@onnexglobal.com>
2026-07-16 17:35:12 +0200

    Remove temporary debug endpoint

--- api/debug-env.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function handler(req, res) {
-  const relevantKeys = Object.keys(process.env).filter(
-    (k) => /POSTGRES|DATABASE|NEON/i.test(k)
-  );
-  res.status(200).json({ keys: relevantKeys });
-};
